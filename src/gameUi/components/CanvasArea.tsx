f57931adc52067f15d4744b5cae7c8dd27502b76
Shashank <shashanksingh819@gmail.com>
2024-06-24 13:13:46 +0530

    add all 52 cards to deck

--- src/gameUi/components/CanvasArea.tsx
+++ src/gameUi/components/CanvasArea.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useRef } from 'react'
-import { Application, Assets, Container, Sprite } from 'pixi.js'
+import { Application, Assets, Container, Sprite, Texture } from 'pixi.js'
 import '../styles/canvasArea.css'
-import back from '../../assets/front.png' // Ensure the correct path to the asset
+import cardBack from '../../assets/back-card.png' // Ensure the correct path to the asset
+import { AllCards } from '../data/cardData' // Importing the AllCards array
 
 const CanvasArea: React.FC = () => {
   const canvasRef = useRef<HTMLDivElement>(null)
@@ -12,92 +13,52 @@ const CanvasArea: React.FC = () => {
     const app = pixiAppRef.current
     if (!app) return
 
-    app.renderer.resize(window.innerWidth, window.innerHeight)
-
-    // Update card dimensions
-    const cardWidth = app.screen.width * 0.1
-    const cardHeight = cardWidth * 1.5
-    const overlapOffset = cardWidth * 0.4
-    const offsetX = app.screen.width * 0.05
-    const offsetY = app.screen.height * -0.05 // Negative value to move cards upwards
-
-    const container = app.stage.children[0] as Container
-
-    // Update positions
-    container.children.forEach((child, index) => {
-      if (child instanceof Sprite) {
-        child.width = cardWidth
-        child.height = cardHeight
-        if (index < 11) {
-          // Opponent's row
-          child.x =
-            (10 - index) * overlapOffset - app.screen.width * 0.2 + offsetX
-          child.y = app.screen.height * 0.1 + offsetY // Move cards upwards
-        } else if (index === 11) {
-          // Draw pile
-          child.x =
-            app.screen.width / 2 -
-            cardWidth -
-            cardWidth * 0.7 -
-            app.screen.width * 0.2 +
-            offsetX
-          child.y =
-            app.screen.height / 2 - cardHeight / 2 - cardHeight * 1.5 + offsetY // Move cards upwards
-        } else if (index === 12) {
-          // Discard pile
-          child.x =
-            app.screen.width / 2 -
-            cardWidth * 0.1 -
-            app.screen.width * 0.2 +
-            offsetX
-          child.y =
-            app.screen.height / 2 - cardHeight / 2 - cardHeight * 1.5 + offsetY // Move cards upwards
-        } else if (index > 12 && index < 18) {
-          // Player's first row
-          child.x =
-            (index - 13) * overlapOffset -
-            cardWidth * 0.3 -
-            app.screen.width * 0.2 +
-            offsetX
-          child.y =
-            app.screen.height - cardHeight - app.screen.height * 0.3 + offsetY // Move cards upwards
-        } else if (index >= 18 && index < 21) {
-          // Player's second row
-          child.x =
-            (index - 18) * overlapOffset +
-            cardWidth * 1.6 -
-            app.screen.width * 0.2 +
-            offsetX
-          child.y =
-            app.screen.height - cardHeight - app.screen.height * 0.3 + offsetY // Move cards upwards
-        } else if (index >= 21) {
-          // Player's third row
-          child.x =
-            (index - 21) * overlapOffset +
-            cardWidth * 2.8 -
-            app.screen.width * 0.2 +
-            offsetX
-          child.y =
-            app.screen.height - cardHeight - app.screen.height * 0.3 + offsetY // Move cards upwards
-        }
-      }
-    })
+    app.renderer.resize(
+      canvasRef.current!.clientWidth,
+      canvasRef.current!.clientHeight
+    )
 
     // Center the container on the screen
-    container.x = (app.screen.width - container.width) / 2
-    container.y = (app.screen.height - container.height) / 2
+    const container = app.stage.children[0] as Container
+    container.x = (canvasRef.current!.clientWidth - container.width) / 2
+    container.y = (canvasRef.current!.clientHeight - container.height) / 2
+  }
+
+  // Drag event handlers
+  const onDragStart = (event: any) => {
+    const card = event.currentTarget
+    card.data = event.data
+    card.dragging = true
+    card.alpha = 0.5 // Make the card semi-transparent
+  }
+
+  const onDragEnd = (event: any) => {
+    const card = event.currentTarget
+    card.dragging = false
+    card.data = null
+    card.alpha = 1 // Reset the transparency
+  }
+
+  const onDragMove = (event: any) => {
+    const card = event.currentTarget
+    if (card.dragging) {
+      const newPosition = card.data.getLocalPosition(card.parent)
+      card.x = newPosition.x
+      card.y = newPosition.y
+    }
   }
 
   useEffect(() => {
     const initializePixi = async () => {
       // Create a new application
       const app = new Application()
-
-      // Store the app reference to use later for cleanup
       pixiAppRef.current = app
 
       // Initialize the application
-      await app.init({ background: '#1c1c1c', resizeTo: window })
+      await app.init({
+        background: '#191919',
+        resizeTo: canvasRef.current!, // Use non-null assertion
+      })
 
       // Append the application canvas to the canvasRef div
       if (canvasRef.current) {
@@ -109,96 +70,44 @@ const CanvasArea: React.FC = () => {
       const container = new Container()
       app.stage.addChild(container)
 
-      // Load the card texture
-      const texture = await Assets.load(back)
-      console.log('Card texture loaded:', texture)
+      // Load the card back texture
+      const backTexture = await Assets.load(cardBack)
+      console.log('Card back texture loaded:', backTexture)
 
       // Define card dimensions relative to screen size
-      const cardWidth = app.screen.width * 0.15
-      const cardHeight = cardWidth * 1.5
-      const overlapOffset = cardWidth * 0.4 // Adjust overlap relative to card width
+      const cardWidth = app.canvas.width * 0.25
+      const cardHeight = cardWidth * 1.4
 
-      // Offset for moving cards to the right and upwards
-      const offsetX = app.screen.width * 0.2 // Adjust this value for the desired horizontal shift
-      const offsetY = app.screen.height * -0.1 // Negative value for the desired vertical shift
-
-      // Create opponent's row of cards with overlap
-      for (let i = 10; i >= 0; i--) {
+      // Function to create a card sprite
+      const createCard = (texture: Texture, x: number, y: number) => {
         const card = new Sprite(texture)
         card.width = cardWidth
         card.height = cardHeight
-        card.x = i * overlapOffset - app.screen.width * 0.2 + offsetX // Move cards to the right
-        card.y = app.screen.height * 0.1 + offsetY // Move cards upwards
-        container.addChild(card)
+        card.x = x
+        card.y = y
+        card.interactive = true
+
+        card
+          .on('pointerdown', onDragStart)
+          .on('pointerup', onDragEnd)
+          .on('pointerupoutside', onDragEnd)
+          .on('pointermove', onDragMove)
+
+        return card
       }
 
-      // Create draw and discard piles in the center
-      const drawPile = new Sprite(texture)
-      drawPile.width = cardWidth
-      drawPile.height = cardHeight
-      drawPile.x =
-        app.screen.width / 2 -
-        cardWidth -
-        cardWidth * 0.7 -
-        app.screen.width * 0.25 +
-        offsetX // Move to the right
-      drawPile.y =
-        app.screen.height / 2 - cardHeight / 2 - cardHeight * 1.5 + offsetY // Move cards upwards
-      container.addChild(drawPile)
+      // Create the deck of cards at the center
+      const deckX = (app.canvas.width - cardWidth) / 2
+      const deckY = (app.canvas.height - cardHeight) / 2
 
-      const discardPile = new Sprite(texture)
-      discardPile.width = cardWidth
-      discardPile.height = cardHeight
-      discardPile.x =
-        app.screen.width / 2 -
-        cardWidth * 0.1 -
-        app.screen.width * 0.2 +
-        offsetX // Move to the right
-      discardPile.y =
-        app.screen.height / 2 - cardHeight / 2 - cardHeight * 1.5 + offsetY // Move cards upwards
-      discardPile.rotation = 10 * (Math.PI / 180)
-      container.addChild(discardPile)
-
-      // Create player's row of cards
-      for (let i = 0; i < 5; i++) {
-        const card = new Sprite(texture)
-        card.width = cardWidth
-        card.height = cardHeight
-        card.x =
-          i * overlapOffset - cardWidth * 0.3 - app.screen.width * 0.2 + offsetX // Move to the right
-        card.y =
-          app.screen.height - cardHeight - app.screen.height * 0.4 + offsetY // Move cards upwards
+      AllCards.forEach(() => {
+        const card = createCard(backTexture, deckX, deckY)
         container.addChild(card)
-      }
-
-      for (let i = 0; i < 3; i++) {
-        const card = new Sprite(texture)
-        card.width = cardWidth
-        card.height = cardHeight
-        card.x =
-          i * overlapOffset + cardWidth * 1.6 - app.screen.width * 0.2 + offsetX // Move to the right
-        card.y =
-          app.screen.height - cardHeight - app.screen.height * 0.375 + offsetY // Move cards upwards
-        container.addChild(card)
-      }
-
-      for (let i = 0; i < 3; i++) {
-        const card = new Sprite(texture)
-        card.width = cardWidth
-        card.height = cardHeight
-        card.x =
-          i * overlapOffset +
-          cardWidth * 2.8 -
-          app.screen.width * 0.15 +
-          offsetX // Move to the right
-        card.y =
-          app.screen.height - cardHeight - app.screen.height * 0.375 + offsetY // Move cards upwards
-        container.addChild(card)
-      }
+      })
 
       // Center the container on the screen
-      container.x = (app.screen.width - container.width) / 2
-      container.y = (app.screen.height - container.height) / 2
+      container.x = 0 // Ensure container is at 0,0
+      container.y = 0
 
       window.addEventListener('resize', resize)
 
@@ -223,237 +132,7 @@ const CanvasArea: React.FC = () => {
     }
   }, [])
 
-  return <div className='canvas-area' ref={canvasRef}></div>
-}
-
-export default CanvasArea
-
-import React, { useEffect, useRef } from 'react'
-import { Application, Assets, Container, Sprite } from 'pixi.js'
-import '../styles/canvasArea.css'
-import back from '../../assets/front.png' // Ensure the correct path to the asset
-
-const CanvasArea: React.FC = () => {
-  const canvasRef = useRef<HTMLDivElement>(null)
-  const pixiAppRef = useRef<Application | null>(null)
-
-  // Define the resize function outside of the useEffect
-  const resize = () => {
-    const app = pixiAppRef.current
-    if (!app) return
-
-    app.renderer.resize(window.innerWidth, window.innerHeight)
-
-    // Update card dimensions
-    const cardWidth = app.screen.width * 0.1
-    const cardHeight = cardWidth * 1.5
-    const overlapOffset = cardWidth * 0.4
-    const offsetX = app.screen.width * 0.05
-    const offsetY = app.screen.height * -0.05 // Negative value to move cards upwards
-
-    const container = app.stage.children[0] as Container
-
-    // Update positions
-    container.children.forEach((child, index) => {
-      if (child instanceof Sprite) {
-        child.width = cardWidth
-        child.height = cardHeight
-        if (index < 11) {
-          // Opponent's row
-          child.x =
-            (10 - index) * overlapOffset - app.screen.width * 0.2 + offsetX
-          child.y = app.screen.height * 0.1 + offsetY // Move cards upwards
-        } else if (index === 11) {
-          // Draw pile
-          child.x =
-            app.screen.width / 2 -
-            cardWidth -
-            cardWidth * 0.7 -
-            app.screen.width * 0.2 +
-            offsetX
-          child.y =
-            app.screen.height / 2 - cardHeight / 2 - cardHeight * 1.5 + offsetY // Move cards upwards
-        } else if (index === 12) {
-          // Discard pile
-          child.x =
-            app.screen.width / 2 -
-            cardWidth * 0.1 -
-            app.screen.width * 0.2 +
-            offsetX
-          child.y =
-            app.screen.height / 2 - cardHeight / 2 - cardHeight * 1.5 + offsetY // Move cards upwards
-        } else if (index > 12 && index < 18) {
-          // Player's first row
-          child.x =
-            (index - 13) * overlapOffset -
-            cardWidth * 0.3 -
-            app.screen.width * 0.2 +
-            offsetX
-          child.y =
-            app.screen.height - cardHeight - app.screen.height * 0.3 + offsetY // Move cards upwards
-        } else if (index >= 18 && index < 21) {
-          // Player's second row
-          child.x =
-            (index - 18) * overlapOffset +
-            cardWidth * 1.6 -
-            app.screen.width * 0.2 +
-            offsetX
-          child.y =
-            app.screen.height - cardHeight - app.screen.height * 0.3 + offsetY // Move cards upwards
-        } else if (index >= 21) {
-          // Player's third row
-          child.x =
-            (index - 21) * overlapOffset +
-            cardWidth * 2.8 -
-            app.screen.width * 0.2 +
-            offsetX
-          child.y =
-            app.screen.height - cardHeight - app.screen.height * 0.3 + offsetY // Move cards upwards
-        }
-      }
-    })
-
-    // Center the container on the screen
-    container.x = (app.screen.width - container.width) / 2
-    container.y = (app.screen.height - container.height) / 2
-  }
-
-  useEffect(() => {
-    const initializePixi = async () => {
-      // Create a new application
-      const app = new Application()
-
-      // Store the app reference to use later for cleanup
-      pixiAppRef.current = app
-
-      // Initialize the application
-      await app.init({ background: '#1c1c1c', resizeTo: window })
-
-      // Append the application canvas to the canvasRef div
-      if (canvasRef.current) {
-        canvasRef.current.appendChild(app.canvas)
-        console.log('Canvas initialized')
-      }
-
-      // Create and add a container to the stage
-      const container = new Container()
-      app.stage.addChild(container)
-
-      // Load the card texture
-      const texture = await Assets.load(back)
-      console.log('Card texture loaded:', texture)
-
-      // Define card dimensions relative to screen size
-      const cardWidth = app.screen.width * 0.15
-      const cardHeight = cardWidth * 1.5
-      const overlapOffset = cardWidth * 0.4 // Adjust overlap relative to card width
-
-      // Offset for moving cards to the right and upwards
-      const offsetX = app.screen.width * 0.2 // Adjust this value for the desired horizontal shift
-      const offsetY = app.screen.height * -0.1 // Negative value for the desired vertical shift
-
-      // Create opponent's row of cards with overlap
-      for (let i = 10; i >= 0; i--) {
-        const card = new Sprite(texture)
-        card.width = cardWidth
-        card.height = cardHeight
-        card.x = i * overlapOffset - app.screen.width * 0.2 + offsetX // Move cards to the right
-        card.y = app.screen.height * 0.1 + offsetY // Move cards upwards
-        container.addChild(card)
-      }
-
-      // Create draw and discard piles in the center
-      const drawPile = new Sprite(texture)
-      drawPile.width = cardWidth
-      drawPile.height = cardHeight
-      drawPile.x =
-        app.screen.width / 2 -
-        cardWidth -
-        cardWidth * 0.7 -
-        app.screen.width * 0.25 +
-        offsetX // Move to the right
-      drawPile.y =
-        app.screen.height / 2 - cardHeight / 2 - cardHeight * 1.5 + offsetY // Move cards upwards
-      container.addChild(drawPile)
-
-      const discardPile = new Sprite(texture)
-      discardPile.width = cardWidth
-      discardPile.height = cardHeight
-      discardPile.x =
-        app.screen.width / 2 -
-        cardWidth * 0.1 -
-        app.screen.width * 0.2 +
-        offsetX // Move to the right
-      discardPile.y =
-        app.screen.height / 2 - cardHeight / 2 - cardHeight * 1.5 + offsetY // Move cards upwards
-      discardPile.rotation = 10 * (Math.PI / 180)
-      container.addChild(discardPile)
-
-      // Create player's row of cards
-      for (let i = 0; i < 5; i++) {
-        const card = new Sprite(texture)
-        card.width = cardWidth
-        card.height = cardHeight
-        card.x =
-          i * overlapOffset - cardWidth * 0.3 - app.screen.width * 0.2 + offsetX // Move to the right
-        card.y =
-          app.screen.height - cardHeight - app.screen.height * 0.4 + offsetY // Move cards upwards
-        container.addChild(card)
-      }
-
-      for (let i = 0; i < 3; i++) {
-        const card = new Sprite(texture)
-        card.width = cardWidth
-        card.height = cardHeight
-        card.x =
-          i * overlapOffset + cardWidth * 1.6 - app.screen.width * 0.2 + offsetX // Move to the right
-        card.y =
-          app.screen.height - cardHeight - app.screen.height * 0.375 + offsetY // Move cards upwards
-        container.addChild(card)
-      }
-
-      for (let i = 0; i < 3; i++) {
-        const card = new Sprite(texture)
-        card.width = cardWidth
-        card.height = cardHeight
-        card.x =
-          i * overlapOffset +
-          cardWidth * 2.8 -
-          app.screen.width * 0.15 +
-          offsetX // Move to the right
-        card.y =
-          app.screen.height - cardHeight - app.screen.height * 0.375 + offsetY // Move cards upwards
-        container.addChild(card)
-      }
-
-      // Center the container on the screen
-      container.x = (app.screen.width - container.width) / 2
-      container.y = (app.screen.height - container.height) / 2
-
-      window.addEventListener('resize', resize)
-
-      return app
-    }
-
-    const pixiAppPromise = initializePixi()
-
-    return () => {
-      pixiAppPromise.then(() => {
-        if (canvasRef.current) {
-          canvasRef.current.innerHTML = ''
-        }
-        if (pixiAppRef.current) {
-          pixiAppRef.current.destroy(true, {
-            children: true,
-            texture: true,
-          })
-        }
-      })
-      window.removeEventListener('resize', resize)
-    }
-  }, [])
-
-  return <div className='canvas-area' ref={canvasRef}></div>
+  return <div className='canvas-area' ref={canvasRef} style={{ flex: 1 }}></div>
 }
 
 export default CanvasArea
---
+++ src/gameUi/data/cardData.ts
@@ -0,0 +1,107 @@
+import C1 from '../../assets/all-cards/Clubs/1.png'
+import C2 from '../../assets/all-cards/Clubs/2.png'
+import C3 from '../../assets/all-cards/Clubs/3.png'
+import C4 from '../../assets/all-cards/Clubs/4.png'
+import C5 from '../../assets/all-cards/Clubs/5.png'
+import C6 from '../../assets/all-cards/Clubs/6.png'
+import C7 from '../../assets/all-cards/Clubs/7.png'
+import C8 from '../../assets/all-cards/Clubs/8.png'
+import C9 from '../../assets/all-cards/Clubs/9.png'
+import C10 from '../../assets/all-cards/Clubs/10.png'
+import CJ from '../../assets/all-cards/Clubs/J.png'
+import CQ from '../../assets/all-cards/Clubs/Q.png'
+import CK from '../../assets/all-cards/Clubs/K.png'
+import D1 from '../../assets/all-cards/Diamonds/1.png'
+import D2 from '../../assets/all-cards/Diamonds/2.png'
+import D3 from '../../assets/all-cards/Diamonds/3.png'
+import D4 from '../../assets/all-cards/Diamonds/4.png'
+import D5 from '../../assets/all-cards/Diamonds/5.png'
+import D6 from '../../assets/all-cards/Diamonds/6.png'
+import D7 from '../../assets/all-cards/Diamonds/7.png'
+import D8 from '../../assets/all-cards/Diamonds/8.png'
+import D9 from '../../assets/all-cards/Diamonds/9.png'
+import D10 from '../../assets/all-cards/Diamonds/10.png'
+import DJ from '../../assets/all-cards/Diamonds/J.png'
+import DQ from '../../assets/all-cards/Diamonds/Q.png'
+import DK from '../../assets/all-cards/Diamonds/K.png'
+import H1 from '../../assets/all-cards/Hearts/1.png'
+import H2 from '../../assets/all-cards/Hearts/2.png'
+import H3 from '../../assets/all-cards/Hearts/3.png'
+import H4 from '../../assets/all-cards/Hearts/4.png'
+import H5 from '../../assets/all-cards/Hearts/5.png'
+import H6 from '../../assets/all-cards/Hearts/6.png'
+import H7 from '../../assets/all-cards/Hearts/7.png'
+import H8 from '../../assets/all-cards/Hearts/8.png'
+import H9 from '../../assets/all-cards/Hearts/9.png'
+import H10 from '../../assets/all-cards/Hearts/10.png'
+import HJ from '../../assets/all-cards/Hearts/J.png'
+import HQ from '../../assets/all-cards/Hearts/Q.png'
+import HK from '../../assets/all-cards/Hearts/K.png'
+import S1 from '../../assets/all-cards/Spades/1.png'
+import S2 from '../../assets/all-cards/Spades/2.png'
+import S3 from '../../assets/all-cards/Spades/3.png'
+import S4 from '../../assets/all-cards/Spades/4.png'
+import S5 from '../../assets/all-cards/Spades/5.png'
+import S6 from '../../assets/all-cards/Spades/6.png'
+import S7 from '../../assets/all-cards/Spades/7.png'
+import S8 from '../../assets/all-cards/Spades/8.png'
+import S9 from '../../assets/all-cards/Spades/9.png'
+import S10 from '../../assets/all-cards/Spades/10.png'
+import SJ from '../../assets/all-cards/Spades/J.png'
+import SQ from '../../assets/all-cards/Spades/Q.png'
+import SK from '../../assets/all-cards/Spades/K.png'
+
+export const AllCards = [
+  C1,
+  C2,
+  C3,
+  C4,
+  C5,
+  C6,
+  C7,
+  C8,
+  C9,
+  C10,
+  CJ,
+  CQ,
+  CK,
+  D1,
+  D2,
+  D3,
+  D4,
+  D5,
+  D6,
+  D7,
+  D8,
+  D9,
+  D10,
+  DJ,
+  DQ,
+  DK,
+  H1,
+  H2,
+  H3,
+  H4,
+  H5,
+  H6,
+  H7,
+  H8,
+  H9,
+  H10,
+  HJ,
+  HQ,
+  HK,
+  S1,
+  S2,
+  S3,
+  S4,
+  S5,
+  S6,
+  S7,
+  S8,
+  S9,
+  S10,
+  SJ,
+  SQ,
+  SK,
+]
