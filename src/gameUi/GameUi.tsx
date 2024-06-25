2765b1ef582010a85eafb6a127d40699f90c6d53
Shashank <shashanksingh819@gmail.com>
2024-06-25 13:39:55 +0530

    improve canvas

--- src/gameUi/GameUi.tsx
+++ src/gameUi/GameUi.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
+
 import './styles/GameUi.css'
 import CanvasArea from './components/CanvasArea'
 import Bar from './components/Bar'
+
 const GameUi: React.FC = () => {
   return (
     <div className='game-ui'>
-      <Bar position="opponent" />
+      <Bar position='opponent' />
       <div className='canvas-wrapper'>
         <CanvasArea />
       </div>
-      <Bar position="player" />
+      <Bar position='player' />
     </div>
   )
 }
--- src/gameUi/components/Bar.tsx
+++ src/gameUi/components/Bar.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import '../styles/Bar.css';
-import profile from '../../assets/images/Profile_Icon.png';
-import setting_btn from '../../assets/images/Settings Icon.png';
-import wifi_btn from '../../assets/images/Network_Connection.png';
-import knock_pass_btn from '../../assets/images/Knock & Pass Button.png';
+import React from 'react'
+
+import '../styles/Bar.css'
+import profile from '../../assets/images/Profile_Icon.png'
+import setting_btn from '../../assets/images/Settings Icon.png'
+import wifi_btn from '../../assets/images/Network_Connection.png'
+import knock_pass_btn from '../../assets/images/Knock & Pass Button.png'
 
 interface BarProps {
-    position: 'player' | 'opponent'
+  position: 'player' | 'opponent'
 }
 
 const Bar: React.FC<BarProps> = ({ position }) => {
@@ -15,12 +16,16 @@ const Bar: React.FC<BarProps> = ({ position }) => {
       <button className='settings-button'>
         <img className='setting-btn-image' src={setting_btn} alt='setting' />
       </button>
-      <div className={`profile-${position}-icon-outer-container`} >
+      <div className={`profile-${position}-icon-outer-container`}>
         <div className='profile-icon-container'>
           <img className='profile-image' src={profile} alt='profile' />
           {position === 'player' && (
             <div className='knock-pass-wrapper'>
-              <img className='knock-pass-button' src={knock_pass_btn} alt='knock-pass' />
+              <img
+                className='knock-pass-button'
+                src={knock_pass_btn}
+                alt='knock-pass'
+              />
               <span className='knock-text'>Knock</span>
             </div>
           )}
@@ -30,7 +35,7 @@ const Bar: React.FC<BarProps> = ({ position }) => {
         <img className='wifi-btn-image' src={wifi_btn} alt='wifi' />
       </button>
     </div>
-  );
-};
+  )
+}
 
-export default Bar;
+export default Bar
